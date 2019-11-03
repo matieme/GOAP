04103c68165915350acaebf54ddbedcb1604f3f6
Mafiu <mf.milewski@gmail.com>
2019-11-03 00:01:35 -0300

    [ENEMY] New Hellephant enemy added

--- Assets/Scripts/Enemy/HellephantEnemy.cs
+++ Assets/Scripts/Enemy/HellephantEnemy.cs
@@ -6,10 +6,8 @@ using GameUtils;
 using System;
 using System.Linq;
 
-public class ZombunnyEnemy : EntityMovement
+public class HellephantEnemy : EntityMovement
 {
-    public GameObject shootParticle;
-    public bool isLaserLoaded;
     public float rushingSpeed;
     public float rotationSpeed;
     public int meleeDamage;
@@ -17,14 +15,10 @@ public class ZombunnyEnemy : EntityMovement
 
     public float pickUpDistance;
     public int clipSize;
-    public LayerMask batteryLayerMask;
     public LayerMask mediKitLayerMask;
 
     [SerializeField]
     private ParticleSystem biteEffect;
-
-    [SerializeField]
-    private EnemyShooting enemyShooting;
 
     private Queue<string> _queueActions = new Queue<string>();
     private Dictionary<string, Func<bool>> concreteActions = new Dictionary<string, Func<bool>>();
@@ -32,34 +26,31 @@ public class ZombunnyEnemy : EntityMovement
 
     private Transform _target;
     private Transform _player;
-    private Transform _closestBattery;
     private Transform _closestMediKit;
     private Animator _anim;
     private bool _finishAnimation;
-    private int _clipSizeMax;
 
     public override void Start()
     {
         base.Start();
         _anim = GetComponent<Animator>();
         _player = GameObject.FindGameObjectWithTag(StringTagManager.tagPlayer).transform;
-        _clipSizeMax = clipSize;
         SetTarget(_player);
         CreateConcreteActionsDict();
         GetGOAPPlaning();
         _vel = Vector3.zero;
         OnReachDestination += OnReachDestinationStopMoving;
-        OnDeath += OnZombunnyDeath;
+        OnDeath += OnHellephantDeath;
     }
 
     private void FixedUpdate()
     {
         if (_target == null || _isDeath) return;
 
-        if(!rotateInPath)
+        if (!rotateInPath)
             Rotate();
 
-        if(_canMove)
+        if (_canMove)
             transform.position += _vel * Time.deltaTime * _movementSpeed;
     }
 
@@ -67,33 +58,7 @@ public class ZombunnyEnemy : EntityMovement
     List<IGOAPAction> CreateActions()
     {
         var _availableActions = new List<IGOAPAction>();
-        
-        _availableActions.Add(
-            new GOAPAction(GOAPActionKeyEnum.Shoot)
-                .SetPrecondition(GOAPKeyEnum.isLaserLoaded, x => (bool)x == true)
-                .SetPrecondition(GOAPKeyEnum.isInWeaponRange, x => (bool)x == true)
-                .SetPrecondition(GOAPKeyEnum.battery, x => (int)x > 0)
-                .SetEffect(GOAPKeyEnum.isAttacking, x => x = true)
-                .SetCost(1));
 
-        _availableActions.Add(
-            new GOAPAction(GOAPActionKeyEnum.PositionToShoot)
-                .SetPrecondition(GOAPKeyEnum.battery, x => (int)x > 0)
-                .SetEffect(GOAPKeyEnum.isInWeaponRange, x => x = true)
-                .SetCost(Math.Max(Vector3.Distance(_player.position, transform.position) - enemyShooting.shootDistance, 1)));
-
-        _availableActions.Add(
-            new GOAPAction(GOAPActionKeyEnum.Reload)
-                .SetPrecondition(GOAPKeyEnum.battery, x => (int)x > 0)
-                .SetEffect(GOAPKeyEnum.isLaserLoaded, x => x = true)
-                .SetCost(1));
-                
-        _availableActions.Add(
-            new GOAPAction(GOAPActionKeyEnum.GrabBattery)
-                .SetPrecondition(GOAPKeyEnum.batteryNearby, x => (bool)x == true)
-                .SetEffect(GOAPKeyEnum.battery, x => x = (int)x + 1)
-                .SetCost(GetCostForBatteryNearby()));
-        
         _availableActions.Add(
             new GOAPAction(GOAPActionKeyEnum.Melee)
                 .SetPrecondition(GOAPKeyEnum.haveCriticalLife, x => (bool)x == false)
@@ -123,13 +88,9 @@ public class ZombunnyEnemy : EntityMovement
         var actions = CreateActions();
         var initialState = new Map<GOAPKeyEnum, object>()
         {
-            { GOAPKeyEnum.battery , battery},
             { GOAPKeyEnum.life , Life},
             { GOAPKeyEnum.isAttacking , false},
-            { GOAPKeyEnum.isLaserLoaded , isLaserLoaded},
             { GOAPKeyEnum.isInMeleeRange , IsInMeleeRange() },
-            { GOAPKeyEnum.isInWeaponRange , IsInShootingRange() },
-            { GOAPKeyEnum.batteryNearby , IsBatteryNearby() },
             { GOAPKeyEnum.medikitNearby , IsMedikitNearby() },
             { GOAPKeyEnum.playerIsAlive , IsPlayerAlive() },
             { GOAPKeyEnum.haveCriticalLife , IsInCriticalLife() }
@@ -168,12 +129,8 @@ public class ZombunnyEnemy : EntityMovement
         GOAPState state = (GOAPState)curr;
 
         if (state.goalValues.ContainsKey(GOAPKeyEnum.isAttacking)) total += (bool)state.currentValues[GOAPKeyEnum.isAttacking] ? 0 : 1;
-        if (state.goalValues.ContainsKey(GOAPKeyEnum.isLaserLoaded)) total += (bool)state.currentValues[GOAPKeyEnum.isLaserLoaded] ? 0 : 1;
         if (state.goalValues.ContainsKey(GOAPKeyEnum.isInMeleeRange)) total += (bool)state.currentValues[GOAPKeyEnum.isInMeleeRange] ? 0 : 1;
-        if (state.goalValues.ContainsKey(GOAPKeyEnum.isInWeaponRange)) total += (bool)state.currentValues[GOAPKeyEnum.isInWeaponRange] ? 0 : 1;
-        if (state.goalValues.ContainsKey(GOAPKeyEnum.batteryNearby)) total += (bool)state.currentValues[GOAPKeyEnum.batteryNearby] ? 0 : 1;
         if (state.goalValues.ContainsKey(GOAPKeyEnum.medikitNearby)) total += (bool)state.currentValues[GOAPKeyEnum.medikitNearby] ? 0 : 1;
-        if (state.goalValues.ContainsKey(GOAPKeyEnum.battery)) total += (int)state.currentValues[GOAPKeyEnum.battery] > 0 ? 0 : 1;
         if (state.goalValues.ContainsKey(GOAPKeyEnum.life)) total += (float)state.currentValues[GOAPKeyEnum.life] > 5 ? 0 : 1;
         if (state.goalValues.ContainsKey(GOAPKeyEnum.haveCriticalLife)) total += (bool)state.currentValues[GOAPKeyEnum.haveCriticalLife] ? 0 : 1;
 
@@ -184,10 +141,6 @@ public class ZombunnyEnemy : EntityMovement
     {
         concreteActions.Add(GOAPActionKeyEnum.Rush.ToString(), Rush);
         concreteActions.Add(GOAPActionKeyEnum.Melee.ToString(), Melee);
-        concreteActions.Add(GOAPActionKeyEnum.GrabBattery.ToString(), GrabBattery);
-        concreteActions.Add(GOAPActionKeyEnum.Reload.ToString(), Reload);
-        concreteActions.Add(GOAPActionKeyEnum.PositionToShoot.ToString(), PositionToShoot);
-        concreteActions.Add(GOAPActionKeyEnum.Shoot.ToString(), Shoot);
         concreteActions.Add(GOAPActionKeyEnum.GetHealed.ToString(), GetHealed);
         return concreteActions;
     }
@@ -195,33 +148,15 @@ public class ZombunnyEnemy : EntityMovement
     #endregion
 
     #region Event Functions
-    public void AnimShootLaser()
-    {
-        if (battery <= 0) return;
-        enemyShooting.Shoot();
-        battery--;
-        _clipSizeMax--;
-    }
-
     public void AnimGrab()
-    {     
+    {
         if (_target == null) return;
 
-        if (_target.GetComponent<DuracellBattery>() != null)
-        {
-            ReplenishBattery(_target.GetComponent<DuracellBattery>().GetBattery());
-        }
-        else if (_target.GetComponent<MedikitBox>() != null)
+        if (_target.GetComponent<MedikitBox>() != null)
         {
             Healed(_target.GetComponent<MedikitBox>().GetHealed());
         }
         _finishAnimation = true;
-    }
-
-    public void AnimReload()
-    {
-        isLaserLoaded = true;
-        _clipSizeMax = clipSize;
     }
 
     public void AnimMeleeAttack()
@@ -230,7 +165,7 @@ public class ZombunnyEnemy : EntityMovement
 
         var victim = _target.GetComponent<PlayerHealth>();
 
-        if (victim != null && Vector3.Distance(_target.position, transform.position) <= meleeDistance * 2)
+        if (victim != null && Vector3.Distance(_target.position, transform.position) <= meleeDistance * 6)
         {
             victim.TakeDamage(meleeDamage);
         }
@@ -272,7 +207,7 @@ public class ZombunnyEnemy : EntityMovement
     }
 
 
-    private void OnZombunnyDeath()
+    private void OnHellephantDeath()
     {
         _isDeath = true;
     }
@@ -300,7 +235,7 @@ public class ZombunnyEnemy : EntityMovement
 
     private bool IsPlayerAlive()
     {
-        return PlayerHealth.Instance.currentHealth > 0; 
+        return PlayerHealth.Instance.currentHealth > 0;
     }
 
     private bool IsInMeleeRange()
@@ -308,47 +243,9 @@ public class ZombunnyEnemy : EntityMovement
         return _target != null ? Vector3.Distance(_target.position, transform.position) <= meleeDistance : false;
     }
 
-    private bool IsInShootingRange()
-    {
-        bool targetInSight = true;
-        if (_target != null)
-        {
-            Vector3 dirToTarget = _target.transform.position - transform.position;
-            RaycastHit[] rch;
-            rch = Physics.RaycastAll(transform.position, dirToTarget, enemyShooting.shootDistance);
-            for (int i = 0; i < rch.Length; i++)
-            {
-                RaycastHit hit = rch[i];
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer(StringTagManager.maskShootable))
-                    targetInSight = false;
-            }
-        }
-        return _target != null ? Vector3.Distance(_target.position, transform.position) <= enemyShooting.shootDistance && targetInSight: false;
-    }
-
     private bool IsInPickUpRange()
     {
         return _target != null ? Vector3.Distance(_target.position, transform.position) <= 2.3f : false;
-    }
-
-    private bool IsBatteryNearby()
-    {
-        return _closestBattery != null;
-    }
-
-    private float GetCostForBatteryNearby()
-    {
-        var duracellBattery = Physics.OverlapSphere(transform.position, pickUpDistance, batteryLayerMask, QueryTriggerInteraction.Collide);
-        if (duracellBattery.Length > 0)
-        {
-            _closestBattery = duracellBattery[0].transform;
-            return Vector3.Distance(_closestBattery.position, transform.position);
-        }
-        else
-        {
-            _closestBattery = null;
-            return 99;
-        }
     }
 
     private bool IsMedikitNearby()
@@ -391,11 +288,12 @@ public class ZombunnyEnemy : EntityMovement
 
         RunOnceAction(initial);
 
-        if(!IsInMeleeRange())
+        if (!IsInMeleeRange())
         {
             return false;
         }
-        else{
+        else
+        {
             _movementSpeed = movementSpeed;
             _anim.speed = 1;
             hasRun = false;
@@ -465,119 +363,6 @@ public class ZombunnyEnemy : EntityMovement
         else
         {
             return false;
-        }
-    }
-
-    private bool GrabBattery()
-    {
-        Action initial = () => {
-            var batteries = BatteryManager.Instance.batteries;
-            batteries.OrderBy(x => Vector3.Distance(x.transform.position, transform.position));
-            if (batteries.Count > 0)
-            {
-                _closestBattery = batteries[0].transform;
-                SetTarget(_closestBattery);
-                SetMove(true);
-                Move();
-                _finishAnimation = false;
-            }
-        };
-
-        RunOnceAction(initial);
-
-        if (IsInPickUpRange() && !_finishAnimation)
-        {
-            SetMove(false);
-            StopMoving();
-            AnimGrab();
-            _anim.SetTrigger(StringTagManager.animPickUp);
-            return false;
-        }
-        else if (_finishAnimation)
-        {
-            _anim.ResetTrigger(StringTagManager.animPickUp);
-            hasRun = false;
-            return true;
-        }
-        else if (_closestBattery == null)
-        {
-            GetGOAPPlaning();
-            hasRun = false;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-    }
-
-    private bool Reload()
-    {
-        Action initial = () =>
-        {
-            SetMove(false);
-            Rotate();
-            _finishAnimation = false;
-            _anim.SetTrigger(StringTagManager.animReload);
-        };
-
-        RunOnceAction(initial);
-
-        if(_finishAnimation)
-        {
-            hasRun = false;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    private bool PositionToShoot()
-    {
-        Action initial = () =>
-        {
-            SetTarget(_player);
-            SetMove(true);
-            Move();
-        };
-
-        RunOnceAction(initial);
-
-        if(IsInShootingRange())
-        {
-            hasRun = false;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    private bool Shoot()
-    {
-        Action initial = () =>
-        {
-            SetTarget(_player);
-            Rotate();
-            SetMove(false);
-        };
-
-        RunOnceAction(initial);
-
-        if(battery > 0 && _clipSizeMax > 0)
-        {
-            return false;
-        }
-        else
-        {
-            isLaserLoaded = false;
-            AnimShootLaser();
-            hasRun = false;
-            return true;
         }
     }
 
